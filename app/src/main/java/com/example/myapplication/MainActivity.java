1e0020f63a839247664e4f19b8feb1047375ea94
andy111617 <andy860606@yahoo.com.tw>
2019-07-07 11:54:14 +0800

    toast

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Toast 基本用法", Toast.LENGTH_LONG).show();
+
+    }
+}
